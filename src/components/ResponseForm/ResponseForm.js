29e47f7acf94ed98c8a6b07bdc34b3b860fe2030
sumedharkulkarni <sumedha.rk.05@gmail.com>
2020-06-08 14:25:30 +0530

    Added JSON Formatting and eval validation

--- src/components/ResponseForm/ResponseForm.js
+++ src/components/ResponseForm/ResponseForm.js
@@ -1,24 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 import "./ResponseForm.scss";
 import { Form } from "react-bootstrap";
 import ButtonComponent from "../Button/Button";
 
 function ResponseForm(props) {
+  const [mockResponseParams, setParams] = useState({
+    httpResponseName: "",
+    httpHeader: {},
+    httpResponse: null
+  });
+
+  const [showHeaderError, setHeaderErrorFlag] = useState(false);
+  const [showResponseError, setResponseErrorFlag] = useState(false);
+
+  const checkJSONErrors = (jsonObj) => {
+    try {
+      eval(JSON.parse(jsonObj));
+    } catch {
+      return false;
+    }
+    return true;
+  };
+
+  const setMockResponseParam = (paramName, value) => {
+    let newResponseObject = Object.assign({}, mockResponseParams);
+    const stringifiedValue = JSON.stringify(value);
+    let hasValidJSON = false;
+
+    switch (paramName) {
+      case "httpResponseName":
+        newResponseObject.httpResponseName = value;
+        break;
+      case "httpHeader":
+        hasValidJSON = checkJSONErrors(stringifiedValue) && eval(value);
+        if (hasValidJSON) {
+          newResponseObject.httpHeader = value;
+        }
+        setHeaderErrorFlag(!hasValidJSON);
+
+        break;
+      case "httpResponse":
+        hasValidJSON = checkJSONErrors(stringifiedValue) && eval(value);
+        if (hasValidJSON) {
+          newResponseObject.httpResponse = value;
+        }
+        setResponseErrorFlag(!hasValidJSON);
+
+        break;
+      default:
+        break;
+    }
+
+    setParams(newResponseObject);
+  };
+
   return (
     <div className="response-form-container">
       <Form>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group controlId="httpResponseName">
           <Form.Label>Mock Response Name</Form.Label>
-          <Form.Control type="text" />
+          <Form.Control
+            type="text"
+            onChange={(e) =>
+              setMockResponseParam("httpResponseName", e.target.value)
+            }
+          />
         </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>HTTP headers</Form.Label>
-          <Form.Control as="textarea" rows="5" />
+        <Form.Group controlId="httpHeader">
+          <Form.Label>HTTP Headers</Form.Label>
+          {showHeaderError && (
+            <Form.Text className="error-text-muted">
+              Error encountered. Please check the HTTP Header Object.
+            </Form.Text>
+          )}
+          <Form.Control
+            as="textarea"
+            rows="5"
+            onChange={(e) => setMockResponseParam("httpHeader", e.target.value)}
+          />
         </Form.Group>
-        <Form.Group controlId="formBasicEmail">
-          <Form.Label>HTTP response</Form.Label>
-          <Form.Control as="textarea" rows="20" />
+        <Form.Group controlId="httpResponse">
+          <Form.Label>HTTP Response</Form.Label>
+          {showResponseError && (
+            <Form.Text className="error-text-muted">
+              Error encountered. Please check the HTTP Response Object.
+            </Form.Text>
+          )}
+          <Form.Control
+            as="textarea"
+            rows="20"
+            onChange={(e) =>
+              setMockResponseParam("httpResponse", e.target.value)
+            }
+          />
         </Form.Group>
         <div className="create-btn-container">
           <ButtonComponent variant="primary" text="Create Mock Response" />
